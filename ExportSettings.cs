47136b79c9f1d8bdff511413ac0b8d4374e848ec
itamochi <omochi6644@gmail.com>
2018-04-08 13:46:07 +0900

    find objects around camera

---
+++ ExportSettings.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace gta5_vision_data_extractor
+{
+    public enum SaveObjectType
+    {
+        PEDESTRIANS,
+        VEHICLES,
+        PED_AND_VEHICLES,
+    };
+
+    /// <summary>
+    /// Serialized detection setting info
+    /// </summary>
+    [DataContract]
+    public class ExportDetectionSettings
+    {
+        /// <summary>
+        /// Output result directory
+        /// </summary>
+        [DataMember]
+        public string OutputDirectory = "default_save_dir";
+
+        /// <summary>
+        /// Type of save objects
+        /// </summary>
+        [DataMember]
+        public SaveObjectType SaveObjectType = SaveObjectType.PEDESTRIANS;
+
+        /// <summary>
+        /// Detection data is saved per this frame span
+        /// </summary>
+        [DataMember]
+        public int SaveFrameSpan = 60;
+
+        /// <summary>
+        /// Find object max distance
+        /// </summary>
+        [DataMember]
+        public float DetectDistance = 100.0f;
+
+        /// <summary>
+        /// Serialize to json file
+        /// </summary>
+        /// <param name="json_path"></param>
+        public void SerializeToJson(string json_path)
+        {
+            var setting = new System.Runtime.Serialization.Json.DataContractJsonSerializerSettings();
+            using (var fs = new FileStream(json_path, FileMode.OpenOrCreate))
+            {
+                fs.Seek(0, SeekOrigin.Begin);
+                var serializer = new DataContractJsonSerializer(typeof(ExportDetectionSettings), setting);
+                serializer.WriteObject(fs, this);
+            }
+        }
+
+        /// <summary>
+        /// Load Settings from serialized json file
+        /// </summary>
+        /// <param name="json_path"></param>
+        /// <returns></returns>
+        public static ExportDetectionSettings DeserializeFromJsonOrDefault(string json_path)
+        {
+            if (System.IO.File.Exists(json_path))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(ExportDetectionSettings));
+                using (var fs = new FileStream(json_path, FileMode.Open))
+                {
+                    var ret = serializer.ReadObject(fs) as ExportDetectionSettings;
+                    return ret;
+                }
+            }
+            else
+            {
+                // if file not exist, return default instance
+                return new ExportDetectionSettings();
+            }
+        }
+    }
+}
--- MainScript.cs
+++ MainScript.cs
@@ -15,12 +15,30 @@ namespace gta5_vision_data_extractor
     /// </summary>
     public class ObjectDetector : Script
     {
+
+        /// <summary>
+        /// Settings of detection
+        /// </summary>
+        private ExportDetectionSettings _save_settings = null;
+
+        /// <summary>
+        /// Tick called counts
+        /// </summary>
+        private int _frame_count = 0;
+
+
         /// <summary>
         /// Constructor
         /// </summary>
         public ObjectDetector()
         {
+            // load setting from json file
+            _save_settings = ExportDetectionSettings.DeserializeFromJsonOrDefault("scripts/vision_data_extractor_settings.json");
+
+
+            // set event handlers
             this.Tick += onTick;
+
         }
 
         /// <summary>
@@ -30,8 +48,35 @@ namespace gta5_vision_data_extractor
         /// <param name="e"></param>
         private void onTick(object sender, EventArgs e)
         {
+            _frame_count++;
 
+            // check if this frame is target frame
+            if (_frame_count % _save_settings.SaveFrameSpan != 0) return;
+
+            // find target objects around camera
+            var camera_position = GameplayCamera.Position;
+
+            List<Entity> found_entities = new List<Entity>();
+
+            // find pedestrians
+            if (_save_settings.SaveObjectType == SaveObjectType.PEDESTRIANS ||
+                _save_settings.SaveObjectType == SaveObjectType.PED_AND_VEHICLES)
+            {
+                found_entities.AddRange(World.GetNearbyPeds(camera_position, _save_settings.DetectDistance).ToList());
+            }
+
+            // find vehicles
+            if (_save_settings.SaveObjectType == SaveObjectType.VEHICLES ||
+                _save_settings.SaveObjectType == SaveObjectType.PED_AND_VEHICLES)
+            {
+                found_entities.AddRange(World.GetNearbyVehicles(camera_position, _save_settings.DetectDistance).ToList());
+            }
+
+            // detect 2D bounding box
+
+            // override to image
+
+            // save image
         }
-
     }
 }
