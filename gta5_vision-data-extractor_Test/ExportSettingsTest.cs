2c9a700f56eaa4c48bd85828905bb0d1a2484c90
itamochi <omochi6644@gmail.com>
2018-04-08 18:26:13 +0900

    add test project

---
+++ gta5_vision-data-extractor_Test/ExportSettingsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using gta5_vision_data_extractor;
+
+namespace gta5_vision_data_extractor_Test
+{
+    [TestClass]
+    public class ExportSettingsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new ExportDetectionSettings();
+        }
+    }
+}
---
+++ gta5_vision-data-extractor_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gta5_vision-data-extractor_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gta5_vision-data-extractor_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("230af5c6-dc35-4a0b-b078-8f2ca9a8a021")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
