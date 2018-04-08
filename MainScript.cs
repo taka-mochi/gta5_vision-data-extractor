331e0fab7cb7d0685ec095ae94b9e2c6c7b4a06a
itamochi <omochi6644@gmail.com>
2018-04-08 18:05:30 +0900

    consider pedestrian bone positions. save results as an image

--- MainScript.cs
+++ MainScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 using GTA;
 using GTA.Native;
@@ -26,6 +27,11 @@ namespace gta5_vision_data_extractor
         /// </summary>
         private int _frame_count = 0;
 
+        /// <summary>
+        /// Time when script started (to be used for output filename prefix)
+        /// </summary>
+        private DateTime _script_started_time;
+
 
         /// <summary>
         /// Constructor
@@ -35,9 +41,10 @@ namespace gta5_vision_data_extractor
             // load setting from json file
             _save_settings = ExportDetectionSettings.DeserializeFromJsonOrDefault("scripts/vision_data_extractor_settings.json");
 
+            _script_started_time = DateTime.Now;
 
             // set event handlers
-            this.Tick += onTick;
+            this.Tick += OnTick;
 
         }
 
@@ -46,21 +53,21 @@ namespace gta5_vision_data_extractor
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void onTick(object sender, EventArgs e)
+        private void OnTick(object sender, EventArgs e)
         {
             _frame_count++;
 
             // check if this frame is target frame
             if (_frame_count % _save_settings.SaveFrameSpan == 0)
             {
-                doDetectionExport();
+                DoDetectionExport();
             }
         }
 
         /// <summary>
-        /// Perform detection data export
+        /// Perform detection data export (Toolchain method: call GTA5 native functions & utility functions)
         /// </summary>
-        private void doDetectionExport()
+        private void DoDetectionExport()
         {
             // find target objects around camera
             var camera_position = GameplayCamera.Position;
@@ -81,16 +88,77 @@ namespace gta5_vision_data_extractor
                 found_entities.AddRange(World.GetNearbyVehicles(camera_position, _save_settings.DetectDistance).ToList());
             }
 
+            List<GTAUtils.BoundingBox2D> peds_bblist = new List<GTAUtils.BoundingBox2D>();
+            List<GTAUtils.BoundingBox2D> vehicle_bblist = new List<GTAUtils.BoundingBox2D>();
+
             // detect 2D bounding box
+            foreach (var entity in found_entities)
+            {
+                // check if is in screen
+                if (!entity.IsVisible) continue;
+                if (!entity.IsOnScreen) continue;
+                if (entity.IsOccluded) continue;
+
+                // check occlusion by ray-casting
+                // ...
+
+                var bb = GTAUtils.ComputeBoundingBox(entity);
+
+                // cannot get bounding box
+                if (bb == null) continue;
+
+                if (entity.Model.IsPed)
+                {
+                    peds_bblist.Add(bb);
+                }
+                else
+                {
+                    vehicle_bblist.Add(bb);
+                }
+            }
+
+            var screenshot = Utils.GetPrimaryScreenImage();
 
             // override to image
+            var overdrawn_image = new Bitmap(screenshot);
+            DrawBoundingBoxes(overdrawn_image, peds_bblist, Color.Red);
+            DrawBoundingBoxes(overdrawn_image, vehicle_bblist, Color.Blue);
 
             // save image
+            var filename_prefix = _script_started_time.ToString("yyyy-MM-dd_hh-mm-ss_");
+            var filepath_prefix = System.IO.Path.Combine(_save_settings.OutputDirectory, filename_prefix);
+            var filenumber_str = string.Format("{0:D8}", _frame_count);
+            Utils.SaveBitmap(overdrawn_image, filepath_prefix + "with_rect_" + filenumber_str + ".jpg");
+            Utils.SaveBitmap(screenshot, filepath_prefix + "original_" + filenumber_str + ".png");
         }
 
+        /// <summary>
+        /// Draw all bounding boxes to the given image
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <param name="bbs"></param>
+        /// <param name="draw_color"></param>
+        private void DrawBoundingBoxes(Bitmap bitmap, List<GTAUtils.BoundingBox2D> bbs, Color draw_color)
+        {
+            int width = bitmap.Size.Width;
+            int height = bitmap.Size.Height;
 
-        //--------------------- temporaliry util code ------
-        
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(draw_color, 2);
+
+            foreach (var bb in bbs)
+            {
+                int x = (int)(width * bb.min.X);
+                int y = (int)(height * bb.min.Y);
+                int w = (int)(width * (bb.max.X - bb.min.X));
+                int h = (int)(height * (bb.max.Y - bb.min.Y));
+
+                g.DrawRectangle(pen, new Rectangle(x, y, w, h));
+            }
+
+            pen.Dispose();
+            g.Dispose();
+        }
 
 
         
---
+++ Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace gta5_vision_data_extractor
+{
+    /// <summary>
+    /// Generic util class
+    /// </summary>
+    public class Utils
+    {
+        /// <summary>
+        /// Create and return the current fullscreen image
+        /// </summary>
+        /// <returns></returns>
+        public static Bitmap GetPrimaryScreenImage()
+        {
+            var primaryScreen = Screen.PrimaryScreen;
+
+            // create new bitmap
+            int width = primaryScreen.Bounds.Width;
+            int height = primaryScreen.Bounds.Height;
+            Bitmap sc_image = new Bitmap(width, height);
+
+            // copy from screen
+            var graphic = Graphics.FromImage(sc_image);
+            graphic.CopyFromScreen(0, 0, 0, 0, new Size(width, height));
+            graphic.Dispose();
+
+            return sc_image;
+        }
+
+        /// <summary>
+        /// Save the image to the path as an image file
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="save_path"></param>
+        public static void SaveBitmap(Image image, string save_path)
+        {
+            image.Save(save_path);
+        }
+    }
+}
